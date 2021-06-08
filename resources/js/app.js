4d523deb2dcd5fa0eb17015e56d55c2307c8014b
Nicolay <krjuchkovkolja@mail.ru>
2021-06-08 17:57:27 +0300

    Start front

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ window.Vue = require('vue').default;
 import  BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueCharts from 'vue-chartjs'
 
+Vue.use(VueCharts);
 Vue.use(BootstrapVue);
 Vue.component('example-component', require('./components/ModelsTable.vue').default);
 
--- resources/js/components/ModelsTable.vue
+++ resources/js/components/ModelsTable.vue
@@ -1,18 +1,33 @@
 <template>
     <div class="container">
         <b-table striped hover :items="modelsData"></b-table>
+        <canvas ref="myChart" width="800" height="300"></canvas>
     </div>
 </template>
 
 <script>
+import Chart from 'chart.js';
     export default {
         data() {
             return {
-                modelsData: []
+                modelsData: [],
+
             }
         },
         mounted(){
             this.getModules();
+            new Chart(this.$refs.myChart, {
+                type: 'line',
+                data: {
+                    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+                    datasets: [
+                    {
+                        label: 'График скачиваний модуля',
+                        data: [300, 700, 450, 750, 450]
+                    }
+                    ]
+                }
+            });
         },
         methods:{
             async getModules(){
@@ -20,6 +35,7 @@
                 const response = await axios.get(`/can`)  
                     console.log(response);
                     this.modelsData = response.data;
+
                     }
                     catch{
 
