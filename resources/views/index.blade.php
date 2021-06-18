dc872fd77733bc6838b4b0d031894d556f4f5dc2
Nicolay <krjuchkovkolja@mail.ru>
2021-06-18 14:18:59 +0300

    Final

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-<<<<<<< HEAD
-    <title>Модули конкурентов</title>
-=======
     <link href="css/app.css" rel="stylesheet">
-    <title>Modules</title>
->>>>>>> b1d6a783df18e8c557fdf29e8099a56bce7174cf
+    <title>Модули конкурентов</title>
 </head>
 <body style="margin: 0 !important; padding: 0 !important;">
     <div id="app">
